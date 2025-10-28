03061fb0d052037297ce9540472130519733bab5
Nandini <Nandinipatel0307@gmail.com>
2025-10-29 00:01:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const sentence = prompt('Enter a Sentence: ');
+const oldWord = prompt('Enter the word you want to replace');
+const newWord = prompt('Enter the word you want to replace with');
+const subString = prompt ('Enter the substring you want to check')
+const wordCount = sentence.split(' ').length
+
+const replaceWord = sentence.replace(oldWord, newWord);
+
+const checkSubstring = sentence.includes(subString)
+
+const ans  = `Sentence: ${sentence}<br>Number of words in this sentence: ${wordCount}<br>After replacement of words: ${replaceWord}<br>Is the word in this sentence? ${checkSubstring}`
+document.getElementById('out').innerHTML = ans
